83afb6d17bb93ebae16aada95c29f99a8e2fdc79
aspyo <sungpyo8334@naver.com>
2024-09-15 01:49:23 +0900

    [FEAT] : 댓글 생성, 삭제 기능 구현
    1. 댓글 생성, 삭제 기능 구현

--- src/main/java/hello/board/domain/comment/presentation/CommentController.java
+++ src/main/java/hello/board/domain/comment/presentation/CommentController.java
@@ -1,6 +1,11 @@
 package hello.board.domain.comment.presentation;
 
 import hello.board.domain.comment.application.CommentService;
+import hello.board.domain.comment.domain.Comment;
+import hello.board.domain.post.application.PostService;
+import hello.board.domain.user.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -14,6 +19,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class CommentController {
 
     private final CommentService commentService;
+    private final PostService postService;
+
+    /**
+     * 댓글 생성 메서드
+     */
+    @PostMapping("/comment")
+    public String createComment(@RequestParam("content") String content,
+                                @RequestParam("post_id") Long postId,
+                                HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("loginUser") == null) {
+            return "redirect:/login";
+        }
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        commentService.createComment(loginUser, postService.findPost(postId), content);
+
+        return "redirect:" + request.getHeader("Referer");
+    }
 
     /**
      * 게시글 삭제 요청 처리 메서드
